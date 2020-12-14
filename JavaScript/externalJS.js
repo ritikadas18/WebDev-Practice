85f48c123686088b337232e38df5454cc1450426
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-12-14 19:57:00 +0530

    updated

---
+++ JavaScript/externalJS.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("container");
+        x.style.width="250px";
+        x.style.height="250px";
+        x.style.backgroundColor="red";
+        x.innerHTML="First JavaScript Code";
+        x.style.textAlign="center";
+        x.style.lineHeight="200px";
+var y= document.getElementById("btn");
+btn.onclick=function()
+{
+    alert("Power of JAVASCRIPT");
+    x.style.backgroundColor="green";
+};
