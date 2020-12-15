9b1ab7152bca150dcb6ed7f49e3507f7636510f7
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-12-15 17:19:16 +0530

    updated

---
+++ JavaScript/function.js
@@ -0,0 +1,12 @@
+function myfun()
+{
+    var name = "Ritika";
+    console.log("My name is"+name);
+}
+myfun()
+
+ function myfun2(fname,lname=" ")
+ {
+    console.log("My name is"+fname+" "+lname);
+ }
+ myfun2("Ritika")
