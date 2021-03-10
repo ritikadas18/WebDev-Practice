6b8aaee8a020d8f11a41aeda189a8b7670457495
Ritika Das <48116271+ritikadas18@users.noreply.github.com>
2021-03-11 00:14:34 +0530

    react state +  update

---
+++ ReactJS/ReactState.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default class ReactState extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            roll:this.props.roll,
+            name:this.props.name,
+        };
+    };
+    UpdateMe=()=>{
+        this.setState({
+            roll:10,
+            name:"ritika das",
+        });
+    };
+    render(){
+        return(
+            <div>
+                <h1>{this.state.roll}</h1>
+                <h1>{this.state.name}</h1>
+                <button onClick={this.UpdateMe}>Update me</button>
+            </div>
+        );
+    }
+}
