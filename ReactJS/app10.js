fdd4790d18f10444beb340b7c5fd8d4db2716240
Ritika Das <48116271+ritikadas18@users.noreply.github.com>
2021-03-11 10:42:22 +0530

    Create app10.js

---
+++ ReactJS/app10.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import FunctionalComponent from './components/FunctionalComponent';
+import ClassComponent from './components/ClassComponent';
+import LifeCycleHooks from './components/LifeCycleHooks';
+function App() {
+    return ( 
+      
+        <LifeCycleHooks/>
+     
+    );
+}
+export default App;
